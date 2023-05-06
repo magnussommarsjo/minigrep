6d9cf7e35ef907194d86d55264e9a2ad0653c02f
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-05-06 20:17:08 +0200

    Add error handling for run function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs;
 use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,14 +14,19 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    run(config)
+    if let Err(e) = run(config){
+        print!("Application error: {e}");
+        process::exit(1)
+    };
 }
 
-fn run(config: Config) {
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+        fs::read_to_string(config.file_path)?;
 
     println!("With contents:\n{contents}");
+
+    Ok(())
 }
 
 struct Config {
