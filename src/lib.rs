f00852f9ca7b2530c863226124814f249b39719b
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-05-07 09:47:33 +0200

    Add function body of search_case_insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -24,7 +24,14 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
 }
 
 pub fn seach_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()){
+            results.push(line)
+        }
+    }
+
+    results
 } 
 
 pub struct Config {
