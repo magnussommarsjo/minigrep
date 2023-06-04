e1b957c1dcdc7ea44eb2d29faf2eb4293735e50c
Magnus Sommarsjö <magnus.lengquist@gmail.com>
2023-06-04 08:13:52 +0200

    Refactor Config::build() to accept and make use of Iterator trait.

--- src/lib.rs
+++ src/lib.rs
@@ -48,12 +48,18 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next(); // Skipping name of the program
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path")
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
